6689cc047bc0b7567446990a1f511311f8e6a281
Diar Kundakbayev <kundakbaev.d@gmail.com>
2021-10-16 23:55:23 +0600

    Initial Commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //Создаем таблицу с юзерами
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
